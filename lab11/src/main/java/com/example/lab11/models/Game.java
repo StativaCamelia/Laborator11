b9647a191b7c6151d29a4322e8b6f9a18aa00c8d
Georgiana <stativa50@gmail.com>
2020-05-04 00:19:32 +0300

    Admin auth

--- lab11/src/main/java/com/example/lab11/models/Game.java
+++ lab11/src/main/java/com/example/lab11/models/Game.java
@@ -1,5 +1,30 @@
 package com.example.lab11.models;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
 public class Game
 {
+    @Id
+    public ObjectId _id;
+    public String content;
+    LocalDate myObj = LocalDate.now();
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public ObjectId get_id() {
+        return _id;
+    }
+
+    public void set_id(ObjectId _id) {
+        this._id = _id;
+    }
 }
